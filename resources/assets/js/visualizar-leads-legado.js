495044d9ecb0a4f1d554191f27ad75b5b375da2f
celso romao <pepitobatista5@gmail.com>
2024-10-04 18:03:38 -0300

    corrigindo dataTables

--- resources/assets/js/visualizar-leads-legado.js
+++ resources/assets/js/visualizar-leads-legado.js
@@ -6,6 +6,7 @@
 
 $(function () {
   var dt_product_table = $('.datatables-products');
+
   // E-commerce Products datatable
   if (dt_product_table.length) {
     var dt_products = dt_product_table.DataTable({
@@ -56,66 +57,67 @@ $(function () {
           });
       }
     });
-  }
 
-  $('.js-click-button-modal').on('click', function () {
-    var idMailing = $(this).data('idmailing');
+    // Usando delegação de eventos para o botão "Visualizar"
+    dt_product_table.on('click', '.js-click-button-modal', function () {
+      var idMailing = $(this).data('idmailing');
 
-    $('#getDate').modal('show');
+      $('#getDate').modal('show');
 
-    fetch('/mailing/getLeadsLegacy/' + idMailing)
-      .then(response => response.json())
-      .then(data => {
-        $('#name').val(data.contato.nameClient || '');
-        $('#email').val(data.contato.email || '');
-        $('#cpf').val(data.contato.cpf || '');
-        $('#phone1').val(data.contato.phone1 || '');
-        $('#phone2').val(data.contato.phone2 || '');
-        $('#phone3').val(data.contato.phone3 || '');
-        $('#ages').val(data.contato.birthDate || '');
-        $('#plan').val(data.contato.plan || '');
-        $('#prince').val(data.contato.prince || '');
-        $('#situation').val(data.contato.situation || '');
-        $('#entity').val(data.contato.entity || '');
-        $('#category').val(data.contato.category || '');
-        let comments = data.comentarios || [];
-        let commentsList = $('.js-list-commentsOne');
-        commentsList.empty();
+      fetch('/mailing/getLeadsLegacy/' + idMailing)
+        .then(response => response.json())
+        .then(data => {
+          $('#name').val(data.contato.nameClient || '');
+          $('#email').val(data.contato.email || '');
+          $('#cpf').val(data.contato.cpf || '');
+          $('#phone1').val(data.contato.phone1 || '');
+          $('#phone2').val(data.contato.phone2 || '');
+          $('#phone3').val(data.contato.phone3 || '');
+          $('#ages').val(data.contato.birthDate || '');
+          $('#plan').val(data.contato.plan || '');
+          $('#prince').val(data.contato.prince || '');
+          $('#situation').val(data.contato.situation || '');
+          $('#entity').val(data.contato.entity || '');
+          $('#category').val(data.contato.category || '');
+          let comments = data.comentarios || [];
+          let commentsList = $('.js-list-commentsOne');
+          commentsList.empty();
 
-        comments.forEach(comment => {
-          var comentario = comment.notes || '';
-          var autor = comment.name || '';
+          comments.forEach(comment => {
+            var comentario = comment.notes || '';
+            var autor = comment.name || '';
 
-          const date = new Date(comment.dateCreate);
-          const formattedDate = date.toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit'
+            const date = new Date(comment.dateCreate);
+            const formattedDate = date.toLocaleDateString('pt-BR', {
+              day: '2-digit',
+              month: '2-digit',
+              year: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit'
+            });
+
+            let commentHtml = `
+              <ul class="timeline pb-0 mb-0">
+                  <li class="timeline-item timeline-item-transparent border-primary">
+                      <span class="timeline-point timeline-point-primary"></span>
+                      <div class="timeline-event">
+                          <div class="timeline-header mb-1">
+                              <h6 class="mb-0">Feito por:
+                                  <span class="badge bg-label-success">${autor}</span>
+                              </h6>
+                              <small class="text-muted">${formattedDate}</small>
+                          </div>
+                          <p class="mt-1 mb-3">${comentario}</p>
+                      </div>
+                  </li>
+              </ul>`;
+
+            commentsList.append(commentHtml);
           });
-
-          let commentHtml = `
-                <ul class="timeline pb-0 mb-0">
-                    <li class="timeline-item timeline-item-transparent border-primary">
-                        <span class="timeline-point timeline-point-primary"></span>
-                        <div class="timeline-event">
-                            <div class="timeline-header mb-1">
-                                <h6 class="mb-0">Feito por:
-                                    <span class="badge bg-label-success">${autor}</span>
-                                </h6>
-                                <small class="text-muted">${formattedDate}</small>
-                            </div>
-                            <p class="mt-1 mb-3">${comentario}</p>
-                        </div>
-                    </li>
-                </ul>`;
-
-          commentsList.append(commentHtml);
+        })
+        .catch(error => {
+          console.error('Erro ao buscar os dados do mailing:', error);
         });
-      })
-      .catch(error => {
-        console.error('Erro ao buscar os dados do mailing:', error);
-      });
-  });
+    });
+  }
 });
