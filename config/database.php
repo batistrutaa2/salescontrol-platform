a9d967b9e517b0df849225060fbe3ac130ef913b
celso romao <pepitobatista5@gmail.com>
2025-06-16 11:58:57 -0300

    adicionando nova conexão MySQL ao arquivo de configuração do banco de dados

--- config/database.php
+++ config/database.php
@@ -78,6 +78,27 @@ return [
       ]) : [],
     ],
 
+
+    'mysql3' => [
+      'driver' => 'mysql',
+      'host' => env('DB_PERSONAL_HOST', '127.0.0.1'),
+      'port' => env('DB_PERSONAL_PORT', '3306'),
+      'database' => env('DB_PERSONAL_DATABASE', 'forge'),
+      'username' => env('DB_PERSONAL_USERNAME', 'forge'),
+      'password' => env('DB_PERSONAL_PASSWORD', ''),
+      'unix_socket' => env('DB_SOCKET', ''),
+      'charset' => env('DB_CHARSET', 'utf8mb4'),
+      'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+      'prefix' => '',
+      'prefix_indexes' => true,
+      'strict' => true,
+      'engine' => null,
+      'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+      ]) : [],
+    ],
+
+
     'mariadb' => [
       'driver' => 'mariadb',
       'url' => env('DB_URL'),
